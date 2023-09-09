86625827e5b5b05e566f24160c7ff1774181dd59
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2023-09-09 15:26:36 +0700

    firstCommit

---
+++ JQuery/main.js
@@ -0,0 +1,41 @@
+// jQuery
+
+/*
+jQuery adalah kumpulan fungsi2 Js yang memudahkan penulisan kode Js
+
+jQuery mempunyai fitur seperti menyederhanakan :
+a. Document Travesing
+b. Event Handling
+c. Animating
+d. Interaksi AJAX
+
+Digunakan untuk pengembangan web secara cepat
+
+John Resig, creator/original author dari jQuery
+
+Gunanya jQuery :
+1. Menemukan elemen dalam dokumen HTML
+2. Mengubah konten HTML
+3. Mendengarkan apa yang dilakukan pengguna dan lakukan
+    tindakan yang sesuai (event listener)
+4. Membuat animasi konten di halaman
+5. Berbicara melalui jaringan untuk mengambil konten baru. (AJAX)
+
+DOM --> akses elemen HTML
+
+Cara memanggil jQuery 
+panggil file jQuery --> dari CDN
+Karena Js (dan jQuery) dapat berjalan sebelum DOM "siap", maka kita
+jalankan (listen) kode jQuery jika dokumen (DOM) sudah siap (ready)
+*/
+
+// -------------------------------------------------------------------------------------------------------
+
+/*
+Menggunakan jQuery Selector
+CSS vs jQuery
+p {...}             <-->     $("p");
+#container {...}    <-->     $("#container");
+.articles {...}     <-->     $(".articles");
+
+*/
